bbb38b0c59b5639ccfaa1ca448e1f8ffaebb4846
vinhnguyenx <vinh.nguyen09102003@gmail.com>
2025-04-11 13:58:54 -0700

    final version

--- models/distilbert_model.py
+++ models/distilbert_model.py
@@ -6,12 +6,12 @@ class DistilBERTClassifier(nn.Module):
     def __init__(self, num_classes):
         super(DistilBERTClassifier, self).__init__()
         self.encoder = DistilBertModel.from_pretrained("distilbert-base-uncased")
-        # self.dropout = nn.Dropout(0.3)  # Dropout layer removed
+        self.dropout = nn.Dropout(0.3) #dropout
         self.classifier = nn.Linear(self.encoder.config.hidden_size, num_classes)
 
     def forward(self, input_ids, attention_mask):
         outputs = self.encoder(input_ids=input_ids, attention_mask=attention_mask)
         pooled_output = outputs.last_hidden_state[:, 0, :]
-        # pooled_output = self.dropout(pooled_output)  # Dropout application removed
+        pooled_output = self.dropout(pooled_output)  
         logits = self.classifier(pooled_output)
         return logits
